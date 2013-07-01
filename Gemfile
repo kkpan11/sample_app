79addb2dc97b2e5f1921a3ade49e283c37ec9acc
Gianni Hong <kkpan1100@gmail.com>
2013-07-01 20:22:41 +0800

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -31,6 +31,9 @@ group :test do
 	# gem 'win32console', '1.3.2'
 
 	gem 'factory_girl_rails', '4.2.1'
+
+	gem 'cucumber-rails', '1.3.0', :require => false
+	gem 'database_cleaner', '~> 1.0.0.rc2'
 end
 
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			sign_in @user
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to @user
 		else
@@ -20,5 +21,9 @@ class UsersController < ApplicationController
 	private
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
 		end
 end
